79c9d6482a716627719c7c59e3b26719586e8366
mohdhuzaifa <mohdhuzaifa8126195456@gmail.com>
2023-06-13 11:43:11 +0530

    database path changes

--- server/controller/services/routes/database/mogoose.js
+++ server/controller/services/routes/database/mogoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 //  const  url ="mongodb+srv://mohdhuzaifakhan:mohdhuzaifakhan@cluster0.o0unkga.mongodb.net/employee?retryWrites=true&w=majority"
-const url = "mongodb://0.0.0.0:27017/users";
+const url = "mongodb+srv://mohdhuzaifakhan:mohdhuzaifakhan@cluster0.o0unkga.mongodb.net/users?retryWrites=true&w=majority";
 mongoose.connect(url,{
     useNewUrlParser: true
 }).then(()=>{
